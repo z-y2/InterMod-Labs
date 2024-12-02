59cf76c100c0925b5d774ebac13e2fd60d5f5d7e
Noah McIvor <noahmcivor@gmail.com>
2024-12-02 12:41:07 -0500

    formatting

--- file-converter/src/Home.jsx
+++ file-converter/src/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"
-    
 
 export const Home = () => {
 
@@ -16,28 +15,28 @@ export const Home = () => {
     if (event.target.files.length > 0) {
       const selectedFileNames = Array.from(event.target.files).map(file => file.name);
       setFileNames(selectedFileNames);
-      navigate("/convert", {state: {fileNames: selectedFileNames}});
+      navigate("/convert", { state: { fileNames: selectedFileNames } });
     }
   };
 
   return (
     <main>
-        <section id="converter-interface">
-          <div id="background">
-            <img src="/images/start_page.png" alt="Background" />
-          </div>
-          <div id="rec_file_input" onClick={triggerFileUpload}></div>
-          <input
-            type="file"
-            id="fileInput"
-            style={{ display: 'none' }}
-            accept=".txt, .csv, .json, .png, .jpg, .jpeg, .heic"
-            onChange={handleFileChange}
-          />
-        </section>
-        <div id="title">
-          <img src="/images/bottom1.png" alt="Ultimate File Converter" />
+      <section id="converter-interface">
+        <div id="background">
+          <img src="/images/start_page.png" alt="Background" />
         </div>
+        <div id="rec_file_input" onClick={triggerFileUpload}></div>
+        <input
+          type="file"
+          id="fileInput"
+          style={{ display: 'none' }}
+          accept=".txt, .csv, .json, .png, .jpg, .jpeg, .heic"
+          onChange={handleFileChange}
+        />
+      </section>
+      <div id="title">
+        <img src="/images/bottom1.png" alt="Ultimate File Converter" />
+      </div>
     </main>
   );
 }
